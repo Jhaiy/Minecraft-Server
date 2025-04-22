09f5ae46c11e62c860d8738469de27ac37e83ccf
Valentino III S. Vista <vista.valentino.samson@gmail.com>
2025-04-22 18:21:56 +0000

    New commit

--- install-important.sh
+++ install-important.sh
@@ -5,5 +5,12 @@ chmod +x *
 
 curl -SsL https://playit-cloud.github.io/ppa/key.gpg | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/playit.gpg >/dev/null
 echo "deb [signed-by=/etc/apt/trusted.gpg.d/playit.gpg] https://playit-cloud.github.io/ppa/data ./" | sudo tee /etc/apt/sources.list.d/playit-cloud.list
+curl -sSL https://ngrok-agent.s3.amazonaws.com/ngrok.asc \
+  | sudo tee /etc/apt/trusted.gpg.d/ngrok.asc >/dev/null \
+  && echo "deb https://ngrok-agent.s3.amazonaws.com buster main" \
+  | sudo tee /etc/apt/sources.list.d/ngrok.list \
+  && sudo apt update \
+  && sudo apt install ngrok
 sudo apt update
 sudo apt install playit
+ 
