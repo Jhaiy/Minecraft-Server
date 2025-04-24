59cf29d45ccbbdefa094853e94c548da7b45b4e2
Valentino III S. Vista <vista.valentino.samson@gmail.com>
2025-04-24 18:55:46 +0000

    Safer for browser hopping

--- start-server.sh
+++ start-server.sh
@@ -3,21 +3,10 @@
 
 sudo chmod +x *
 
-sudo apt update && apt upgrade
+sudo apt update
 
 screen -S server-terminal -d -m ./run.sh
 
 screen -S ngrok-terminal -d -m ngrok tcp 25565
 
-screen -S playit-terminal -d -m playit 
-
-echo "====================="
-echo "Sending IP Address..."
-echo "====================="
-sleep 5
-
-echo "====================="
-echo "--------SENT!--------"
-echo "====================="
-
-./discord-webhook.sh
+screen -S playit-terminal -d -m playit
