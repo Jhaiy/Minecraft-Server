4e4109e00a526abaedad31e1a60f03aa0e2aa77b
Valentino III S. Vista <vista.valentino.samson@gmail.com>
2025-04-26 01:58:34 +0000

    test

--- start-server.sh
+++ start-server.sh
@@ -7,6 +7,6 @@ sudo apt update
 
 screen -S server-terminal -d -m ./run.sh
 
-screen -S ngrok-terminal -d -m ngrok tcp 25565
+screen -S ngrok-terminal -d -m ssh -R 80:localhost:25565 serveo.net
 
 screen -S playit-terminal -d -m playit
--- stop-server.sh
+++ stop-server.sh
@@ -1,24 +1,2 @@
 #!/bin/bash
-
-sudo killall ngrok
-
 sudo killall screen
-
-discord_url="https://discordapp.com/api/webhooks/1318779458131460147/gXLgsFnRSaGgeuI9NlSDTvCmgaUe-9_ImyosPcBHu-cGpbaVDq1UylUKCW0yutuzzHXg"
-
-generate_post_data() {
-  cat <<EOF
-{
-  "content": "",
-  "embeds": [{
-    "title": "SERVER STATUS:",
-    "description": "Server Closed",
-    "color": "45973"
-  }]
-}
-EOF
-}
-
-
-# POST request to Discord Webhook
-curl -H "Content-Type: application/json" -X POST -d "$(generate_post_data)" $discord_url
