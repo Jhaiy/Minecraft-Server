ef8e12dd82cb5f5fae10740ac9e0d730922c5a2b
Valentino III S. Vista <vista.valentino.samson@gmail.com>
2025-04-28 06:23:00 +0000

    save

--- start-server.sh
+++ start-server.sh
@@ -7,6 +7,4 @@ sudo apt update
 
 screen -S server-terminal -d -m ./run.sh
 
-screen -S serveo-terminal -d -m ssh -R jaiyyhelmc.serveo.net:25565:localhost:25565 serveo.net
-
 screen -S playit-terminal -d -m playit
